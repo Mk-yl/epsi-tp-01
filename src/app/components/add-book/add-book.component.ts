afdca60d8708a25ddbf0606f7dd3f0d329a8cbfc
Mkl <mkly0208@gmail.com>
2025-05-06 16:36:55 +0200

    Ajoute de header et du footer todo 5 a 9 done

--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BookService } from '../../services/book.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-book',
@@ -22,23 +23,36 @@ export class AddBookComponent implements OnInit {
   ngOnInit(): void {
     // TODO 6 : Créer un formulaire avec les champs suivants : title, author, description, category
     // TODO 7 : Ajouter les validations nécessaires
-
     this.bookForm = this.fb.group({
-      title: ['', [Validators.required]],
-      author: ['', [Validators.required]],
-      description: ['', [Validators.required]],
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      author: ['', [Validators.required, Validators.minLength(3)]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
       category: ['', [Validators.required]]
     });
-
   }
 
   onSubmit(): void {
     if (this.bookForm.valid) {
       this.bookService.addBook(this.bookForm.value).subscribe({
         next: () => {
-          this.router.navigate(['/books']);
+          // Utilisation de SweetAlert2 pour afficher une alerte de succès
+          Swal.fire({
+            title: 'Succès!',
+            text: 'Le livre a été ajouté avec succès.',
+            icon: 'success',
+            confirmButtonText: 'OK'
+          }).then(() => {
+            this.router.navigate(['/books']);
+          });
         },
         error: (err: any) => {
+          // Utilisation de SweetAlert2 pour afficher une alerte d'erreur
+          Swal.fire({
+            title: 'Erreur!',
+            text: 'Une erreur est survenue lors de l\'ajout du livre.',
+            icon: 'error',
+            confirmButtonText: 'OK'
+          });
           console.error('Erreur lors de l\'ajout du livre', err);
         }
       });
