ff3ee21ae61dcbc236ce6538a324efa6040b3034
Mkl <mkly0208@gmail.com>
2025-05-06 16:04:33 +0200

    ajoute de pipes et suppression des pipes non necessaire

--- src/app/title-format.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TitleFormatPipe } from './title-format.pipe';
-
-describe('TitleFormatPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TitleFormatPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
