3036919557ca5764b76625a4fc60929d7df67575
Mkl <mkly0208@gmail.com>
2025-05-06 16:26:21 +0200

    Todo 13 a 19 dans la page book liste Done

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { BookService } from '../../services/book.service';
+import { Router, RouterModule } from '@angular/router';
 import { Book } from '../../models/book.model';
-import { Router } from '@angular/router';
-import {HighlightDirective} from '../../directives/highlight.directive';
+import { HighlightDirective } from '../../directives/highlight.directive';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-book-list',
-  standalone: true,
-  imports: [CommonModule, RouterLink, FormsModule, HighlightDirective],
   templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css'],
+  standalone: true,
+  imports: [CommonModule, RouterModule, HighlightDirective]
 })
 export class BookListComponent implements OnInit {
   books: Book[] = [];
@@ -42,9 +42,21 @@ export class BookListComponent implements OnInit {
     this.bookService.toggleFavorite(book.id).subscribe({
       next: (updatedBook: Book) => {
         // TODO 16: Affiche une alerte qui indique que le favori a été modifié
+        Swal.fire({
+          title: 'Succès!',
+          text: 'Le favori a été modifié.',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        });
       },
       error: (err: any) => {
         // TODO 17: Affiche une alerte qui indique que la modification du favori a échoué
+        Swal.fire({
+          title: 'Erreur!',
+          text: 'La modification du favori a échoué.',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
         console.error('Erreur lors de la modification du favori:', err);
       }
     });
@@ -54,10 +66,22 @@ export class BookListComponent implements OnInit {
     this.bookService.deleteBook(id).subscribe({
       next: () => {
         // TODO 18: Affiche une alerte qui indique que le livre a été supprimé
+        Swal.fire({
+          title: 'Succès!',
+          text: 'Le livre a été supprimé.',
+          icon: 'success',
+          confirmButtonText: 'OK'
+        });
         console.log('Livre supprimé:', id);
       },
       error: (err: any) => {
         // TODO 19: Affiche une alerte qui indique que la suppression du livre a échoué
+        Swal.fire({
+          title: 'Erreur!',
+          text: 'La suppression du livre a échoué.',
+          icon: 'error',
+          confirmButtonText: 'OK'
+        });
         console.error('Erreur lors de la suppression du livre:', err);
       }
     });
